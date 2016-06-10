aa66fb702e90a06078f803f0ef0b16063af74b80
Rafael Bernardelli <rafael.bernardelli@gmail.com>
2016-06-10 13:55:54 +0200

    adding files

---
+++ Bilateral Filter GPU/stdafx.h
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <iostream>
+
+// Shared Library Test Functions
+//#include <helper_functions.h>  // CUDA SDK Helper functions
+  
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/gpu/gpu.hpp>
