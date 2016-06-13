7f8ee384ae5395dcd6f715acabbc1697c3f26a46
Rafael Bernardelli <rafael.bernardelli@gmail.com>
2016-06-13 11:33:40 +0200

    Revert "first implementation for readable code"

--- Bilateral Filter GPU/slicing.h
+++
@@ -1,8 +0,0 @@
-#ifndef SLICING_H
-#define SLICING_H
-
-__global__ void slicing(float *dev_image, const float*dev_cube_wi, const float*dev_cube_w, const dim3 imsize);
-
-float* callingSlicing( cv::Mat image, float *dev_cube_wi, float *dev_cube_w);
-
-#endif
